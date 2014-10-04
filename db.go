d8b1e1e6b34a41e095326322a1ada964e3df92af
lalyos <lalyos@yahoo.com>
2014-10-04 12:45:06 +0200

    listTable fund added

--- db.go
+++ db.go
@@ -9,22 +9,57 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	fmt.Println("db ..")
+var db *sql.DB
+var err error
 
+func pingDB() {
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() {
 	dbUrl := os.Getenv("DBURL")
 	if dbUrl == "" {
 		panic("Please set the DBURL env variable: postgres://user:pwd@host/dbname?sslmode=disable")
 	}
 
-	db, err := sql.Open("postgres", dbUrl)
+	db, err = sql.Open("postgres", dbUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	err = db.Ping()
+func listTables() {
+
+	log.Println("[tables]")
+	schema := "public"
+	listTables := `
+	SELECT table_name
+    FROM information_schema.tables
+		WHERE table_schema=$1
+		ORDER BY table_name;
+	`
+
+	rows, err := db.Query(listTables, schema)
 	if err != nil {
 		log.Fatal(err)
 	}
+	var table string
+	for rows.Next() {
+		err := rows.Scan(&table)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(table)
+	}
+}
+
+func main() {
+	openDB()
 	defer db.Close()
+
+	pingDB()
+	listTables()
 }
